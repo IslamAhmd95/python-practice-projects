d211bfd2582e53e11e59cdea63c5b55b0253f00e
IslamAhmd95 <dev.islam95@gmail.com>
2026-01-05 08:36:37 +0200

    django: working on forms

--- 12.django/1.worldtour/asiatoursagency/urls.py
+++ 12.django/1.worldtour/asiatoursagency/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('contact', views.contact_view, name='contact'),
+    path('contact/success', views.contact_success_view, name='contact-success'),
 ]
--- 12.django/1.worldtour/asiatoursagency/views.py
+++ 12.django/1.worldtour/asiatoursagency/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 from .models import Tour
+from .form import ContactForm
+
 
 # Create your views here.
 def index(request):
     tours = Tour.objects.all()
     context = {'tours': tours}
     return render(request, 'tours/index.html', context)
+
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.send_email()
+            return redirect('contact-success')
+    else:
+        form = ContactForm()
+    return render(request, 'contact/index.html', {'form': form})
+
+
+def contact_success_view(request):
+    return render(request, 'contact/contact_success.html')
